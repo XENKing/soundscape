bf0bc5def51300ee61091196311da1a4c9f4e832
XENK <xenkings@gmail.com>
2019-12-19 01:08:05 +0700

    Update MySQL db

--- db.go
+++ db.go
@@ -7,7 +7,7 @@ import (
 	//  _ "github.com/jinzhu/gorm/dialects/postgres"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	//  _ "github.com/jinzhu/gorm/dialects/mssql"
-	//"fmt"
+	"fmt"
 )
 
 // User ...
@@ -22,13 +22,18 @@ var db *gorm.DB
 func dbInit() {
 	var err error
 	// Switch database
-	if os.Getenv("MYSQL_DB") != "" {
-		db, err = gorm.Open("mysql", os.Getenv("MYSQL_USER")+":"+os.Getenv("MYSQL_PASSWORD")+"@tcp("+os.Getenv("MYSQL_HOST")+":"+os.Getenv("MYSQL_PORT")+")/"+os.Getenv("MYSQL_DB")+"?charset=utf8&parseTime=True&loc=Local")
-	} else {
-		db, err = gorm.Open("sqlite3", "./soundscape.db")
+	fmt.Printf("Open db connection: %q : %q\n", os.Getenv("MYSQL_DATABASE"), os.Getenv("MYSQL_USER")+":"+os.Getenv("MYSQL_PASSWORD")+"@tcp("+os.Getenv("DOCKER_DB_CONTAINER_NAME")+")/"+os.Getenv("MYSQL_DATABASE")+"?charset=utf8&parseTime=True&loc=Local")
+	if os.Getenv("MYSQL_DATABASE") != "" {
+		//db, err = gorm.Open("mysql", os.Getenv("MYSQL_USER")+":"+os.Getenv("MYSQL_PASSWORD")+"@tcp("+os.Getenv("MYSQL_ROOT_HOST")+":"+os.Getenv("MYSQL_PORT")+")/"+os.Getenv("MYSQL_DATABASE")+"?charset=utf8&parseTime=True&loc=Local")
+		db, err = gorm.Open("mysql", os.Getenv("MYSQL_USER")+":"+os.Getenv("MYSQL_PASSWORD")+"@tcp("+os.Getenv("DOCKER_DB_CONTAINER_NAME")+")/"+os.Getenv("MYSQL_DATABASE")+"?charset=utf8&parseTime=True&loc=Local")
 	}
 	if err != nil {
-		panic("failed to connect database")
+		fmt.Printf("Error connecting to db: %q\n", err)
+		db, err = gorm.Open("sqlite3", "/data/soundscape.db")
+		if err != nil {
+			fmt.Printf("Error connecting to db: %q\n", err)
+			panic("failed to connect database")
+		}
 	}
 	//defer db.Close()
 	db.SingularTable(true)
